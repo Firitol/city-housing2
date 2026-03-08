266d727133baa8874673660dd55b18e22a658923
Firitol <92720354+Firitol@users.noreply.github.com>
2026-03-08 04:03:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import os
 from app import create_app
 
 app = create_app()
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Use Render's port
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
